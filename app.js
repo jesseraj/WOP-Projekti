d86b331c5760e107853fe46e43fcdc2a80e54425
jesseraj <89629604+jesseraj@users.noreply.github.com>
2021-12-26 16:16:18 +0200

    first commitaaaa

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('./utils/pass');
 const { httpError } = require('./utils/errors');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 app.use(cors());
 
 app.use(express.json()); // for parsing application/json
